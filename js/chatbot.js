609f66c9a01bf4a91212702749545f7cc480caf3
sasha-kravets <skravets.home@gmail.com>
2023-11-28 08:08:20 +0200

    SoftServe JS Project Upload

---
+++ js/chatbot.js
@@ -0,0 +1,96 @@
+const chatbot = document.querySelector(".chatbot");
+const chatInput = chatbot.querySelector(".chatbot__input-textarea");
+const sendChatBtn = chatbot.querySelector(".chatbot__input-btn");
+const chatbox = chatbot.querySelector(".chatbox");
+
+let userMessage = null;
+const API_KEY_PART1 = "sk-af6jzVndp7oLNO7";
+const API_KEY_PART2 = "trMZ2T3BlbkFJT";
+const API_KEY_PART3 = "byEdCba4NVEg6jbpcGm";
+const API_KEY = API_KEY_PART1 + API_KEY_PART2 + API_KEY_PART3;
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+  // Create a chat <li> element with passed message and className
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chatbox__msg", `${className}`);
+
+  let chatContent =
+    className === "chatbox__msg_outgoing"
+      ? `<p class="chatbox__msg-text"></p>`
+      : `<span class="chatbox__msg-icon"><img src="img/openai-icon.svg" alt="OpenAI icon"></span>
+         <p class="chatbox__msg-text"></p>`;
+
+  // Prevents the use of html tags in the input
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector(".chatbox__msg-text").textContent = message;
+  return chatLi; // return chat <li> element
+};
+
+const generateResponse = (incomingChatLi) => {
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  const messageElement = incomingChatLi.querySelector(".chatbox__msg-text");
+
+  // Define the properties and message for the API request
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userMessage }],
+    }),
+  };
+
+  // Send POST request to API and get response
+  fetch(API_URL, requestOptions)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      messageElement.textContent = data.choices[0].message.content;
+    })
+    .catch((error) => {
+      messageElement.classList.add("error");
+      messageElement.textContent =
+        "Oops! Something went wrong. Please try again.";
+      console.log(error);
+    })
+    .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+};
+
+const handleChat = () => {
+  userMessage = chatInput.value.trim();
+  if (!userMessage) return;
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+
+  // Append the user's message to the chatbox
+  chatbox.appendChild(createChatLi(userMessage, "chatbox__msg_outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  setTimeout(() => {
+    // Display "Thinking.." message while waiting for the response
+    const incomingChatLi = createChatLi("Thinking..", "chatbox__msg_incoming");
+    chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    generateResponse(incomingChatLi);
+  }, 600);
+};
+
+chatInput.addEventListener("input", () => {
+  // Adjust the height of the input textarea based on its content
+  chatInput.style.height = `${inputInitHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+sendChatBtn.onclick = () => handleChat();
+
+// Enter and Shift+Enter keys combination
+chatInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && !event.shiftKey && window.innerWidth > 800) {
+    event.preventDefault();
+    handleChat();
+  }
+});
---
+++ js/dalle.js
@@ -0,0 +1,87 @@
+const generateForm = document.querySelector(".images-generator__form");
+const imageGallery = document.querySelector(".image-gallery");
+
+const DALLE_API_KEY_PART1 = "sk-8fGYRpkToK6pX7D";
+const DALLE_API_KEY_PART2 = "GeQvFT3BlbkF";
+const DALLE_API_KEY_PART3 = "JYvieoEWov7IhCRvSvsab";
+const OPENAI_API_KEY = DALLE_API_KEY_PART1 + DALLE_API_KEY_PART2 + DALLE_API_KEY_PART3;
+let isImageGenerating = false;
+
+const updateImageCard = (imgDataArray) => {
+  imgDataArray.forEach((imgObject, index) => {
+    const imgCard = imageGallery.querySelectorAll(".card")[index];
+    const imgElement = imgCard.querySelector(".card__img");
+    const downloadBtn = imgCard.querySelector(".card__download-btn");
+
+    // Set the image source to the AI-generated image data
+    const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
+    imgElement.src = aiGeneratedImage;
+
+    // When the image is loaded, remove the loading class and set download attributes
+    imgElement.onload = () => {
+      imgCard.classList.remove("loading");
+      downloadBtn.setAttribute("href", aiGeneratedImage);
+      downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
+    };
+  });
+};
+
+// Send a request to the OpenAI API to generate images based on user inputs
+const generateAiImages = async (userPrompt, userImgQuantity) => {
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          prompt: userPrompt,
+          n: parseInt(userImgQuantity),
+          size: "512x512",
+          response_format: "b64_json",
+        }),
+      },
+    );
+
+    if (!response.ok)
+      throw new Error("Failed to generate images! Please try again");
+
+    const { data } = await response.json(); // Get data from the response
+    updateImageCard([...data]);
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    isImageGenerating = false;
+  }
+};
+
+const handleFormSubmission = (e) => {
+  e.preventDefault();
+  if (isImageGenerating) return;
+  isImageGenerating = true;
+
+  // Get user input and image quantity values from the form
+  const userPrompt = e.srcElement[0].value;
+  console.log(userPrompt);
+  const userImgQuantity = e.srcElement[1].value;
+
+  // Creating HTML markup for image cards with loading state
+  const imgCardMarkup = Array.from(
+    { length: userImgQuantity },
+    () =>
+      `<div class="image-gallery__card card loading">
+      <img src="img/dalle/loader.svg" alt="AI generated image" class="card__img">
+      <a href="#" class="card__download-btn">
+        <img src="img/dalle/download.svg" alt="download icon" class="card__download-btn-icon">
+      </a>
+    </div>`,
+  ).join("");
+
+  imageGallery.innerHTML = imgCardMarkup;
+  generateAiImages(userPrompt, userImgQuantity);
+};
+
+generateForm.addEventListener("submit", handleFormSubmission);
